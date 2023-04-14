4e3e0c98d89de7906284613651c7ed3cbbc036dd
bleminmujan <mujan.belmin98@gmail.com>
2023-04-15 00:53:07 +0200

    fixes

--- sketch_mar7a/sketch_mar7a.ino
+++ sketch_mar7a/sketch_mar7a.ino
@@ -3,21 +3,20 @@
 #include <WiFiClient.h>
 #include <ArduinoJson.h>
 #include <math.h>
+#define outputpin A0
 
 const char* ssid = "7b05d4";
 const char* password = "286148654";
 String url = "http://192.168.0.18:5164/api/Room/update_temperature";
 unsigned long lastTime = 0;
-unsigned long timerDelay = 2500;
+unsigned long timerDelay = 1000;
 int counter = 1;
-int outputpin= A0;
 int analogValue;
-float millivolts;
 float celsius;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   // Connect to Wi-Fi network
   Serial.print("Connecting to wifi...");
@@ -34,7 +33,7 @@ void loop() {
   if((millis() - lastTime) > timerDelay){
     if(WiFi.status() == WL_CONNECTED){
       digitalWrite(LED_BUILTIN, LOW);
-      delay(1000);
+      delay(100);
       digitalWrite(LED_BUILTIN, HIGH);
       WiFiClient client;
       HTTPClient http;
@@ -46,9 +45,7 @@ void loop() {
       char macStr[18] = "";
       sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
       analogValue = analogRead(outputpin);
-      // millivolts = (analogValue/1024.0) * 3300;
-      // celsius = millivolts/10;
-      celsius = (analogValue * 3.0 / 1023.0) * 100.0;
+      celsius = analogValue * (3.3 / 1024.0) * 100;
       data["Value"] = celsius;
       data["Id"] = macStr;
       serializeJson(data, serializedData);
